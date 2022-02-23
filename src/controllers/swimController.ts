6ae4a830b3816607244d490088ae74d4c963d020
edpackard <edpackard81@gmail.com>
2022-02-23 17:31:03 +0000

    Update swim controller

--- src/controllers/swimController.ts
+++ src/controllers/swimController.ts
@@ -1,13 +1,13 @@
 import * as database from "../data/swimData"
 
 class swimController {
-  static createSwim(lengths: number, pool: string, date: Date) {
+  createSwim(lengths: number, pool: string, date: Date) {
     database.saveData({lengths, pool, date})
   }
 
-  static getAllSwims(): Array<any> {
+  getAllSwims(): Array<any> {
     return database.getData()
   }
 }
 
-export default swimController
+export default new swimController
