5cab0d5486c5f706ec8ce1ab2ba9b8fa902ea1fb
Ed Packard <edward.packard@kinandcarta.com>
2022-02-24 07:57:24 +0000

    Add get single item functionality to database

--- src/data/swimData.ts
+++ src/data/swimData.ts
@@ -3,9 +3,13 @@ import { Swim } from '../models/SwimModel'
 const data: Array<Swim> = [{ id: 1, lengths: 60, pool: "test data", date: new Date}]
 
 export const getAllData = (): Array<Swim> => {
-    return data
-  }
+  return data
+}
+
+export const getData = (id: number): Swim => {
+  return data[id - 1]
+}
 
 export const saveData = (object: Swim) => {
-    data.push(object)
-  }
+  data.push(object)
+}
